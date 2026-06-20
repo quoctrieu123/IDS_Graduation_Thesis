3c743a1b50002fe06f425fe0e2ba41b883e6bbc4
quoctrieu123 <quoctk04@gmail.com>
2026-06-20 22:21:44 +0700

    Restructure graduation thesis project

--- docker_deployment/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data = pd.read_parquet(r'C:\Users\Admin\Downloads\IoT Dataset\CCIOT\saved_preprocessed\data_1s.parquet')
-print(data.columns)
-
-# in ra timestamp_start nhỏ nhất và lớn nhất
-print("Timestamp Start nhỏ nhất:", data['timestamp_start'].min())
-print("Timestamp Start lớn nhất:", data['timestamp_start'].max())
